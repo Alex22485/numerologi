3c48383a1b132417394ab16c1611cd07bbc24a1c
Alex22485 <alex.224.85@mail.ru>
2025-03-06 17:11:25 +0700

    Auth_disign_ready

--- components/BtnAuth.js
+++ components/BtnAuth.js
@@ -2,23 +2,56 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function BtnAuth() {
   return (
-    <View style={styles.conteiner_BtnAuth}>
-      <Text style={styles.conteiner_BtnAuth_Text}>Создать аккаунт</Text>
+    <View style={styles.conteiner_click_Auth}>
+      <View style={styles.conteiner_BtnAuth}>
+        <Text style={styles.conteiner_BtnAuth_Text}>Создать аккаунт</Text>
+      </View>
+      <View style={styles.conteiner_BtnAuth_forgot}>
+        <Text style={styles.conteiner_BtnAuth_forgot_text}>
+          Уже зарегистрировались?
+        </Text>
+        <Text style={styles.conteiner_BtnAuth_forgot_Btn}>Войти</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  conteiner_BtnAuth: {
+  conteiner_click_Auth: {
     width: "85%",
-    backgroundColor: "#CBB171",
-    marginTop: 75,
     alignSelf: "center",
-    borderRadius: 4,
+    marginTop: 49,
+    // backgroundColor: "white",
+  },
+  conteiner_BtnAuth: {
+    backgroundColor: "#CBB171",
+    borderRadius: 10,
   },
   conteiner_BtnAuth_Text: {
     height: 48,
-    textAlign: "center",
+    alignSelf: "center",
     verticalAlign: "middle",
+    color: "white",
+    fontSize: 19,
+    // lineHeight: 22,
+    // fontWeight: "semibold",
+    // fontFamily: "Roboto",
+  },
+  conteiner_BtnAuth_forgot: {
+    marginTop: 8,
+    flexDirection: "row",
+    justifyContent: "flex-end",
+  },
+  conteiner_BtnAuth_forgot_text: {
+    textAlign: "right",
+    fontSize: 13,
+    letterSpacing: 0.24,
+    color: "#361F36",
+  },
+  conteiner_BtnAuth_forgot_Btn: {
+    color: "#856D31",
+    fontSize: 13,
+    letterSpacing: 0.24,
+    fontWeight: "semibold",
   },
 });
--- components/InputAuth.js
+++ components/InputAuth.js
@@ -3,14 +3,17 @@ import { View, StyleSheet, TextInput } from "react-native";
 export default function InputAuth({ placeHolder }) {
   return (
     <View style={styles.inputView}>
-      <TextInput style={styles.inputView_item} placeholder={placeHolder} />
+      <TextInput
+        style={styles.inputView_item}
+        placeholder={placeHolder}
+        placeholderTextColor="#A686A6"
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   inputView: {
-    // flex: 1,
     marginBottom: 29,
     width: "85%",
     alignSelf: "center",
@@ -18,7 +21,12 @@ const styles = StyleSheet.create({
   inputView_item: {
     height: 48,
     backgroundColor: "#D9D9D9",
-    borderRadius: 4,
+    borderRadius: 10,
     borderColor: "#16171D",
+    paddingLeft: 22,
+    fontSize: 17,
+    lineHeight: 22,
+    color: "#361F36",
+    activeOutlineColor: "white",
   },
 });
