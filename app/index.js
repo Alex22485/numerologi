61a4211819cca259b4174f4a8110872e5b4f33c1
Alex22485 <alex.224.85@mail.ru>
2025-03-06 18:30:47 +0700

    create Auth_page

--- app/index.js
+++ app/index.js
@@ -13,8 +13,6 @@ export default function App() {
     }, 3000);
   }, []);
 
-  // {timeView &&  <Auth />}
-
   if (timeView) {
     return <Auth />;
   }
---
+++ assets/components/auth/auth.js
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function authPage() {
+  return (
+    <View>
+      <Text>fdvdfv</Text>
+    </View>
+  );
+}
