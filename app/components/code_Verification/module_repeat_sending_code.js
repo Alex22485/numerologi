5c2a5ba496eb62dddfa424f4cd235163cc8a5cf4
Alex22485 <alex.224.85@mail.ru>
2025-04-05 21:49:06 +0700

    modile_repeat_sending timer 60-59-58 for sent code_Verification

--- app/components/code_Verification/module_repeat_sending_code.js
+++ app/components/code_Verification/module_repeat_sending_code.js
@@ -1,17 +1,45 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import {
+  Color,
   heightWindow,
   Text_App,
   widthWindow,
 } from "../../../components/tokens";
+import { useEffect, useState } from "react";
 
 export default function Module_repeat_sending_code() {
+  const [valueSecond, setValueSecond] = useState(12);
+
+  useEffect(() => {
+    if (valueSecond !== 0) {
+      const interval = setInterval(() => {
+        console.log("таймер");
+        setValueSecond((item) => {
+          console.log("item: ", item);
+          return item - 1;
+        });
+      }, 1000);
+      return () => {
+        console.log("очистка");
+        clearInterval(interval);
+      };
+    }
+  }, [valueSecond]);
   return (
     <View style={styles.contentView}>
       <Text style={styles.text}> Не приходит письмо?</Text>
-      <Text style={styles.text}> Повторить</Text>
-      <Text style={styles.text}> через</Text>
-      <Text style={styles.text}> 60</Text>
+      <TouchableOpacity disabled={valueSecond !== 0 ? true : false}>
+        <Text style={valueSecond !== 0 ? styles.text : styles.text_time_s_UP}>
+          {" "}
+          Повторить
+        </Text>
+      </TouchableOpacity>
+
+      <Text style={styles.text}> через </Text>
+      <Text style={styles.text}>
+        {" "}
+        {valueSecond >= 10 ? valueSecond : "  " + valueSecond}
+      </Text>
       <Text style={styles.text}> с</Text>
     </View>
   );
@@ -22,10 +50,14 @@ const styles = StyleSheet.create({
     width: widthWindow * 0.86,
     marginTop: heightWindow * 0.0086,
     flexDirection: "row",
-    // backgroundColor: "green",
     justifyContent: "flex-end",
   },
   text: {
     fontSize: Text_App.fs_10,
+  },
+  text_time_s_UP: {
+    fontSize: Text_App.fs_10,
+    fontWeight: "bold",
+    color: Color.brown,
   },
 });
--- components/tokens.js
+++ components/tokens.js
@@ -24,6 +24,7 @@ export const Text_App = {
 export const Color = {
   dark_purple: "#361F36",
   red: "#D31D1A",
+  brown: "#CBB171",
 };
 
 export const Input_Text = {
